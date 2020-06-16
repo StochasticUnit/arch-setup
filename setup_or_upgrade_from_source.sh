84115295dcea8de5f1340db5818f415fb6c9fb21
StochasticUnit <lex@flyingnarwhal.org>
2020-06-16 13:30:48 -0700

    Gobuster and John The Ripper

--- setup_or_upgrade_from_source.sh
+++ setup_or_upgrade_from_source.sh
@@ -1,3 +1,46 @@
 # Software installed from source
 
 source ./setup_env.sh
+
+if [ ! -d ${BIN} ]; then
+	mkdir -p ${BIN}
+fi
+
+# John The Ripper
+cd ${BIN}
+
+build_john() {
+	cd src
+	sudo ./configure
+	make -s clean && make -sj4
+}
+
+if [ ! -d john ]; then
+	git clone https://github.com/magnumripper/JohnTheRipper.git john
+	cd john
+	build_john
+	cd ../run
+	echo "alias john='`pwd`/john'" >> $SHELLRC
+else
+	cd john
+	git pull
+	build_john
+fi
+
+# Gobuster
+cd ${BIN}
+
+if [ ! -d gobuster ]; then
+	git clone https://github.com/OJ/gobuster.git
+	cd gobuster
+	make linux
+	go get && go build
+	go install
+else
+	cd gobuster
+	git pull
+	make linux
+	go get && go build
+	go install
+fi
+
--- setup_software.sh
+++ setup_software.sh
@@ -5,5 +5,16 @@ sudo apt install git
 sudo apt install gcc python golang
 
 # General tools
+sudo apt install htop tmux
 
+#
+# Networking Tools
+#
+
+sudo apt install dnsutils
+sudo apt install nmap curl
+sudo apt install openvpn binwalk
+
+# Dev
+sudo apt install libssl-dev
 
