d847dc775227ee48b548c8102d51927872ab7b98
StochasticUnit <lex@flyingnarwhal.org>
2020-06-16 13:43:45 -0700

    Setup Golang

--- setup_software.sh
+++ setup_software.sh
@@ -1,8 +1,19 @@
 # Software installed through apt
 
+. setup_env.sh
+
 # Dev and Languages
 sudo apt install git
-sudo apt install gcc python golang
+sudo apt install gcc python
+
+# Setup Golang
+sudo apt install golang
+if [ -z $(grep "# ADD GO TO PATH" "$SHELLRC") ]; then
+        echo >> $SHELLRC
+	echo "# ADD GO TO PATH" >> $SHELLRC
+	echo "PATH=$PATH:${HOME}/go/bin" >> $SHELLRC
+	echo >> $SHELLRC
+fi
 
 # General tools
 sudo apt install htop tmux
